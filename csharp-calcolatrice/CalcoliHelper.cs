a453c78b82fa3d706f256972aa11335c7a595ce9
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-12 15:09:06 +0200

    Creazione classe CalcoliHelper statica con relativi metodi

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+
+        public static int somma(int addendo1, int addendo2)
+        {
+            return addendo1 + addendo2;
+        }
+        public static double somma(double addendo1, double addendo2)
+        {
+            return addendo1 + addendo2;
+        }
+
+        public static int differenza(int minuendo, int sottraendo)
+        {
+            return minuendo - sottraendo;
+        }
+        public static double differenza(double minuendo, double sottraendo)
+        {
+            return minuendo - sottraendo;
+        }
+
+        public static int moltiplicazione(int moltiplcando, int moltiplicatore)
+        {
+            return moltiplcando * moltiplicatore;
+        }
+        public static double moltiplicazione(double moltiplicando, double moltiplicatore)
+        {
+            return moltiplicando * moltiplicatore;
+        }
+
+        public static int valoreAssoluto(int valore)
+        {
+            return Math.Abs(valore);
+        }
+        public static double valoraAssoluto(double valore)
+        {
+            return Math.Abs(valore);
+        }
+
+        public static int valoreMinimo(int valore1, int valore2)
+        {
+            if(valore1 < valore2)
+            {
+                return valore1;
+            }
+            else if(valore1 > valore2)
+            {
+                return valore2;
+            }
+            else
+            {
+                Console.WriteLine("I valori inseriri sono uguali");
+                return valore1;
+            }                     
+        }
+        public static double valoreMinimo(double valore1, double valore2)
+        {
+            if(valore1 < valore2)
+            {
+                return valore1;
+            }
+            else if(valore1 > valore2)
+            {
+                return valore2;
+            }
+            else
+            {
+                Console.WriteLine("I valori inseriri sono uguali");
+                return valore1;
+            }
+        }
+
+        public static int valoreMassimo(int valore1, int valore2)
+        {
+            if(valore1 < valore2)
+            {
+                return valore2;
+            }
+            else if(valore1 > valore2)
+            {
+                return valore1;
+            }
+            else
+            {
+                Console.WriteLine("I valori inseriri sono uguali");
+                return valore1;
+            }
+        }
+        public static double valoreMassimo(double valore1, double valore2)
+        {
+            if(valore1 < valore2)
+            {
+                return valore2;
+            }
+            else if(valore1 > valore2)
+            {
+                return valore1;
+            }
+            else
+            {
+                Console.WriteLine("I valori inseriri sono uguali");
+                return valore1;
+            }
+        }
+
+        public static int elevazionePotenza(int baseElevazione, int esponente)
+        {
+            if(esponente == 0)
+            {
+                return 1;
+            }else
+            {
+                return baseElevazione * elevazionePotenza(baseElevazione, esponente - 1);
+            }
+        }
+
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using csharp_calcolatrice;
+
+Console.WriteLine("Somma:");
+Console.WriteLine(CalcoliHelper.somma(10, 56));
+Console.WriteLine(CalcoliHelper.somma(23.3, 45.4));
+Console.WriteLine("------------------------------\n");
+
+Console.WriteLine("Differenza:");
+Console.WriteLine(CalcoliHelper.differenza(56, 90));
+Console.WriteLine(CalcoliHelper.differenza(3.4, 15.8));
+Console.WriteLine("------------------------------\n");
+
+Console.WriteLine("Moltiplicazione:");
+Console.WriteLine(CalcoliHelper.moltiplicazione(45, 87));
+Console.WriteLine(CalcoliHelper.moltiplicazione(56.8, 67.9));
+Console.WriteLine("------------------------------\n");
+
+Console.WriteLine("Valore Assolutoo: ");
+Console.WriteLine(CalcoliHelper.valoraAssoluto(-23));
+Console.WriteLine(CalcoliHelper.valoraAssoluto(-56.9));
+Console.WriteLine("------------------------------\n");
+
+Console.WriteLine("Minimo: ");
+Console.WriteLine(CalcoliHelper.valoreMinimo(23, 45));
+Console.WriteLine(CalcoliHelper.valoreMinimo(23.7, 45.9));
+Console.WriteLine("------------------------------\n");
+
+Console.WriteLine("Massimo: ");
+Console.WriteLine(CalcoliHelper.valoreMassimo(23, 45));
+Console.WriteLine(CalcoliHelper.valoreMassimo(23.7, 45.9));
+Console.WriteLine("------------------------------\n");
+
+Console.WriteLine("Elevazione a potenza: ");
+Console.WriteLine(CalcoliHelper.elevazionePotenza(2, 3));
+Console.WriteLine("------------------------------\n");
